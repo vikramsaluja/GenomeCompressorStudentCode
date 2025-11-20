2eed3a87be51c8786bac6c0cfdc4ca64aef78aaa
vikramsaluja <vikram.saluja@menloschool.org>
2025-11-19 17:19:44 -0800

    GenomeCompressor Completed

--- src/GenomeCompressor.java
+++ src/GenomeCompressor.java
@@ -14,7 +14,7 @@
  *  and expanding a genomic sequence using a 2-bit code.
  *
  *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ *  @authorin Wayne
  *  @author Zach Blick
  */
 public class GenomeCompressor {
@@ -24,9 +24,21 @@ public class GenomeCompressor {
      * { A, C, T, G } from standard input; compresses and writes the results to standard output.
      */
     public static void compress() {
+        // Read in the 8 bit sequence
+        String sequence = BinaryStdIn.readString();
 
-        // TODO: complete the compress() method
+        // Create Header for edge cases
+        int header = sequence.length();
 
+        BinaryStdOut.write(header);
+
+        for(int i = 0; i < header; i++){
+            // Current letter
+            char c = sequence.charAt(i);
+            // Call helper function to get the compressed key
+            int key = compressHelper(c);
+            BinaryStdOut.write(key, 2);
+        }
         BinaryStdOut.close();
     }
 
@@ -34,10 +46,51 @@ public class GenomeCompressor {
      * Reads a binary sequence from standard input; expands and writes the results to standard output.
      */
     public static void expand() {
-
-        // TODO: complete the expand() method
+        int header = BinaryStdIn.readInt();
+        // Iterate through the whole sequence
+        for(int i = 0; i < header; i++){
+            int key = BinaryStdIn.readInt(2);
+            // Call helper function and get the letter
+            char c = expandHelper(key);
+            // Print out the letter
+            BinaryStdOut.write(c);
+        }
 
         BinaryStdOut.close();
+    }
+
+    // Helper function for compress
+    public static int compressHelper(char c){
+        // Depending on what the current character is, return a key associated with that letter
+        if(c == 'A'){
+            return 0b00;
+        }
+        else if(c == 'C'){
+            return 0b01;
+        }
+        else if(c == 'G'){
+            return 0b10;
+        }
+        else{
+            return 0b11;
+        }
+    }
+
+    // Helper function to expand the sequence
+    public static char expandHelper(int key){
+        // Return the letter that corresponds with each key
+        if(key == 0b00){
+            return 'A';
+        }
+        else if(key == 0b01){
+            return 'C';
+        }
+        else if(key == 0b10){
+            return 'G';
+        }
+        else {
+            return 'T';
+        }
     }
 
 
